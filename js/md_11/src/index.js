958fdb77ed7b44b6ca3dc0e12b17d479961bd6a4
Clevin <Clevin.pro@gmail.com>
2019-10-25 15:30:44 +0300

    md 11

---
+++ js/md_11/src/index.js
@@ -0,0 +1,142 @@
+import './styles.css';
+
+const interval = 1000;
+const timeOut = 30000;
+const hours = document
+  .querySelector('.hours');
+const minutes = document
+  .querySelector('.minutes');
+const seconds = document
+  .querySelector('.seconds');
+const dateRef = document
+  .querySelector('.date');
+
+function checkNumber(number) {
+  if(number < 10) {
+    return '0' + String(number);
+  }
+  return number;
+}
+
+
+let date = '';
+const DateNow = new Date();
+
+date += DateNow.getDate() + '.';
+date += DateNow.getMonth() + 1 + '.';
+date += DateNow.getFullYear();
+
+console.log('date', date);
+
+dateRef.textContent = date;
+
+// const timerId = setInterval(() => {
+//   const DateNow = new Date();
+//   let time = '';
+//   time += DateNow.getHours() + ':';
+//   time += DateNow.getMinutes() + ':';
+//   time += DateNow.getSeconds();
+//   console.log('time', time);
+
+//   hours.textContent = checkNumber(DateNow.getHours());
+//   minutes.textContent = checkNumber(DateNow.getMinutes());
+//   seconds.textContent = checkNumber(DateNow.getSeconds());
+
+//   // console.log('interval', new Date());
+// }, interval);
+
+// setTimeout(
+//   () => {
+//     clearInterval(timerId);
+//     console.log('clearInterval');
+//   },
+//   timeOut
+// );
+
+
+const contactUs = document.querySelector('.contactus');
+const modalForm = document.querySelector('.modal__form');
+const form = modalForm.querySelector('.modal');
+
+// Show modal
+contactUs.addEventListener('click', () => {
+  modalForm.classList.add('show');
+});
+
+// Close modal
+modalForm.addEventListener('click', (e) => {
+  if(
+    e.currentTarget === e.target ||
+    e.target.dataset.action === 'CLOSE'
+  ) {
+    modalForm.classList.remove('show');
+  }
+});
+
+
+// Submitting form
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // console.log('e.target.elements.name', e.target.elements.name)
+  const elements = e.target.elements;
+  const submData = {
+    name: elements.name.value,
+    email: elements.email.value,
+    phone: elements.phone.value,
+    message: elements.message.value,
+  }
+  console.log('submData', submData);
+});
+
+
+
+console.log('clg2');
+console.log('clg3');
+
+const myPromise = new Promise((resolve, reject) => {
+  // if(date === '25.10.2019') {
+  //   setTimeout(() => {
+
+  //     resolve("It's true");
+  //   }, 4000);
+
+  // } else {
+  //   reject("It's false");
+  // }
+  setTimeout(() => {
+    modalForm.classList.add('show');
+    resolve();
+  }, 4000);
+});
+
+
+function fetch() {
+  let a = 5;
+  if(true) {
+
+    return Promise.resolve(a * a);
+  }
+}
+
+console.log('fetch()', fetch())
+console.log('fetch()', fetch().then(fetchData => console.log('fetchData', fetchData)))
+
+myPromise
+  .then(someData => {
+    // console.log('someData', someData);
+    setTimeout(() => modalForm.classList.remove('show'), 2000);
+    return 10;
+  })
+  .then(newData => console.log('newData', newData))
+  .catch(err => console.log('err', err));
+
+
+  // setTimeout(() => {
+  //   modalForm.classList.add('show');
+  // }, 4000);
+
+  // setTimeout(() => modalForm.classList.remove('show'), 6000);
+
+
+
